b1ff161854be2a85eb1d9c0687e541ba274a03c7
Nenad <nenad.trifunovic@gmail.com>
2024-05-29 09:55:50 +0200

    fddfsds
    fdfdsfsd

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -754,3 +754,17 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
+
+
+
+function exp_label_Pipelinerev2Pipeline_rev2_55_eval_expression(context) {
+    // Pipeline
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return feature.properties['Pipeline'] ;
+    } else {
+        return feature['Pipeline'] ;
+    }
+}
